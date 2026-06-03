dff628c38e31f8336c1ebb5c5d8aedd43d8bad9a
Jordan van Donkelaar <197877@novacollege.nl>
2026-06-03 09:50:06 +0200

    Update album titles in seeder and enhance navigation with store branding

--- database/seeders/AlbumSeeder.php
+++ database/seeders/AlbumSeeder.php
@@ -15,7 +15,7 @@ class AlbumSeeder extends Seeder
     {
         $albums = [
             [
-                'title' => 'The Adventures of Jörmie & Friends',
+                'title' => 'Jörmies Magnum Opus',
                 'artist_id' => 1,
                 'genre_id' => 1,
                 'user_id' => 1,
@@ -25,7 +25,7 @@ class AlbumSeeder extends Seeder
                 'stock' => 42,
             ],
             [
-                'title' => 'Album 2',
+                'title' => 'God of KFC',
                 'artist_id' => 2,
                 'genre_id' => 2,
                 'user_id' => 1,
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,6 +6,7 @@
                 <div class="shrink-0 flex items-center">
                     <a href="{{ auth()->check() ? (Auth::user()->isAdmin() ? route('dashboard.admin') : (Auth::user()->isFreelancer() ? route('dashboard.freelancer') : route('dashboard.client'))) : '/' }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <span class="ml-3 text-lg font-semibold text-gray-800">Music Store</span>
                     </a>
                 </div>
             </div>
